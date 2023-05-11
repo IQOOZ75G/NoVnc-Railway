ed0571fb2218ab75fcdb00fcf45fafc5ed661376
Yash Kumar <104177185+IQOOZ75G@users.noreply.github.com>
2023-05-12 01:04:20 +0530

    Update ngrok_.py

--- ngrok_.py
+++ ngrok_.py
@@ -4,7 +4,7 @@ import json
 from pyngrok import ngrok
 NGROK_APIKEY = os.environ.get("NGROK_APIKEY", "2OqCWfqxhtrbMNyRGDzDCwbneMV_6kCm5ZwBZca919Vi94nPp")
 ngrok.set_auth_token(NGROK_APIKEY)
-uri=ngrok.connect(5900, "tcp")
+uri=ngrok.connect(0000, "tcp")
 open("/work/noVNC/ngrok.txt", "w").write(uri.public_url)
 open("/work/noVNC/ngrok.json", "w").write(json.dumps(uri.data))
 while True:
